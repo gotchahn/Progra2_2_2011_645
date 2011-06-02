979697d29d8f560bf06cdf0cfece2e8e81f7740f
Carlos Gochez <cgotcha@gmail.com>
2011-06-01 19:36:08 -0600

    more tests

--- src/Files/TextFiles.java
+++ src/Files/TextFiles.java
@@ -5,8 +5,7 @@
 
 //package Files;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 /**
@@ -40,6 +39,12 @@ public class TextFiles {
                 case 1:
                     write();
                     break;
+                case 2:
+                    normalRead();
+                    break;
+                case 3:
+                    readScanner();
+                    break;
             }
         }while( op != 4 );
     }
@@ -69,5 +74,33 @@ public class TextFiles {
         catch(IOException e){
             System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    private static void normalRead() {
+
+        FileReader fr = null;
+
+        try{
+            fr = new FileReader( filePath );
+            File file = new File( filePath );
+
+            char buffer[] = new char[ (int)file.length() ];
+            
+            if( fr.read( buffer ) > 0 ){
+                System.out.println("Contenido de Archivo\n----------");
+                System.out.println(buffer);
+            }
+            else
+                System.out.println("No hay nada que leer");
+
+            fr.close();
+        }
+        catch(IOException io){
+            System.out.println("Error: " + io.getMessage());
+        }
+    }
+
+    private static void readScanner() {
+        //TODO HACER
     }
 }
